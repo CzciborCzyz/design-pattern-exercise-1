f45893a98df000516de07b0a58406a1320b6d6a7
test <test>
2019-02-17 12:19:50 +0100

    Do dokonczenia, zadanie

--- src/main/java/chart/BarChart.java
+++ src/main/java/chart/BarChart.java
@@ -125,4 +125,57 @@ public class BarChart {
     public void setDisplayLegend(boolean displayLegend) {
         this.displayLegend = displayLegend;
     }
+
+    public static class BarChartBuilder {
+
+
+        private String title;
+
+        private String fontName;
+        private int fontSize;
+        private String fontColor;
+
+        private int width;
+        private int height;
+        private String backgroundColor;
+
+        private List<String> data;
+
+        private boolean displayTitle;
+        private boolean displayLegend;
+
+        public BarChartBuilder(String title) {
+            this.title = title;
+        }
+
+        public BarChartBuilder withFontName(String fontName) {
+            this.fontName = fontName;
+            return this;
+        }
+
+        public BarChartBuilder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public BarChartBuilder withFontSize(int fontSize) {
+            this.fontSize = fontSize;
+            return this;
+        }
+
+        public BarChartBuilder withFontColor(String fontColor) {
+            this.fontColor = fontColor;
+            return this;
+        }
+
+        public BarChartBuilder withwidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public BarChartBuilder withheight(int height) {
+            this.height = height;
+            return this;
+        }
+    }
 }
